de0ecfb792b2e4ea14befe6af89a8ae67ceecbdb
Rounakd323 <rounakd323@gmail.com>
2025-10-20 02:28:40 +0530

    Logging and exceptions

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import os
+import logging
+from datetime import datetime
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+
+
+logging.basicConfig(
+    filename=logs_path,
+    level=logging.INFO,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
